6a268d76078af9db5b37ee39f16e8e86041c28d7
amarraza312 <amarraza312@gmail.com>
2025-08-10 21:01:13 +0100

    basic templating created

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import Nav from './components/Nav.jsx'
+
+import './components/App.css'
+import './components/Nav.css'
+function App() {
+  return (
+    <>
+      <Nav />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Nav.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Nav.css'
+
+function Nav() {
+  return (
+    <div className='NavWrapper'>
+      <a href='#'>Home</a>
+    </div>
+  )
+}
+
+export default Nav
